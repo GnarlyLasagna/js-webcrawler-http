f9f0e32fa25767f825f421a25e7c915aef05c996
Evan Dolatowski <evandolatowski@yahoo.com>
2024-02-04 23:16:10 -0600

    it actually works

--- crawl.js
+++ crawl.js
@@ -1,25 +1,46 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currentUrl){
-    console.log("crawling...")
+async function crawlPage(baseUrl, currentUrl, pages){
+
+    const baseUrlObj = new URL(baseUrl)
+    const currentUrlObj = new URL(currentUrl)
+    if(baseUrlObj.hostname !== currentUrlObj.hostname){
+    return pages
+    }
+
+    const normalizedCurrentUrl =normalizeUrl(currentUrl)
+    if(pages[normalizedCurrentUrl]>0){
+        pages[normalizedCurrentUrl]++
+        return pages
+    }
+    pages[normalizedCurrentUrl] = 1
+    console.log(`crawling: ${currentUrl}`)
+
     try{
     const resp = await fetch(currentUrl)
 
     if(resp.status > 399){
         console.log(`error in fetch with status code ${resp.status}`)
-        return
+        return pages
     }
 
     const contentType = resp.headers.get("content-type")
     if(!contentType.includes("text/html")){
         console.log(`error non html response in content type ${contentType}`)
-        return
+        return pages
     }
 
-    console.log(await resp.text())
+    const htmlBody = await resp.text()
+    const nextUrls = getUrlsFromHtml(htmlBody, baseUrl)
+
+    for(const nextUrl of nextUrls){
+       pages = await crawlPage(baseUrl, nextUrl, pages) 
+    }
+
     } catch(err) {
     console.log(`error in fetch ${err.message}`)
     }
+    return pages
 }
 
 function getUrlsFromHtml(htmlBody,baseUrl){
@@ -43,7 +64,6 @@ function getUrlsFromHtml(htmlBody,baseUrl){
                     console.log(`error with relative url ${err.message}`)
                 }
             }
-            console.log("current arr",urls)
     }
     return urls
 }
