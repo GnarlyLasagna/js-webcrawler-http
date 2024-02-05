56290321a03c50e7b7ab5f86cdc87c594eda2cde
Evan Dolatowski <evandolatowski@yahoo.com>
2024-02-04 20:33:20 -0600

    adding first test

---
+++ crawl.test.js
@@ -0,0 +1,10 @@
+const {normalizeUrl} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+test('normalizeUrl', ()=>{
+    const input = ''
+    const actual = normalizeUrl(input)
+    const expected = ''
+
+    expect(actual).toEqual(expected)
+}) 
